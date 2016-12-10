7e5ef5c98eab8ae8e08b7bef1b4680f37b1798d8
Chris Lyth <cjlyth@gmail.com>
2016-12-09 23:19:25 -0600

    Initial commit

---
+++ src/main/java/com/autozone/aafapplication/AafapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.autozone.aafapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AafapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AafapplicationApplication.class, args);
+	}
+}
