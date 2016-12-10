39298e8f83799f5379f9b3221b3fca883a82f0fd
Chris Lyth <cjlyth@gmail.com>
2016-12-10 00:50:16 -0600

    updated project config

--- src/main/java/com/autozone/aafapplication/AafapplicationApplication.java
+++ src/main/java/com/autozone/aafapplication/AafapplicationApplication.java
@@ -2,8 +2,11 @@ package com.autozone.aafapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableMongoHttpSession
 public class AafapplicationApplication {
 
 	public static void main(String[] args) {
